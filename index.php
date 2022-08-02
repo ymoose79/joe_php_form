67f19c1fdf31174d73788bb0abc008b8bdbf139a
Justin <76789435+ymoose79@users.noreply.github.com>
2022-08-02 09:08:26 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,21 +1,6 @@
 <?php
 include('config/db_connect.php');
 
-// 'delete' comes from the submit button
-if (isset($_POST['delete'])) {
-
-    $id_to_delete = mysqli_real_escape_string($conn, $_POST['id_to_delete']);
-
-    // $sql = "DELETE FROM `text` WHERE id = $id_to_delete";
-    $sql = "DELETE FROM `text` (`text`) WHERE id = $id_to_delete";
-
-    if (mysqli_query($conn, $sql)) {
-        header('Location: index.php');
-    } else {
-        echo 'query error: ' . mysqli_error($conn);
-    }
-}
-
 // write query to get data
 $sql = 'SELECT * FROM text';
 // make query and get results
